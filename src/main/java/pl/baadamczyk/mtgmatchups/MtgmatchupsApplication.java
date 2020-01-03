d1e1a87342e3ae217672d803b308c06ca9558af9
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2020-01-03 13:05:05 +0100

    fixup! Initial commit

---
+++ src/main/java/pl/baadamczyk/mtgmatchups/MtgmatchupsApplication.java
@@ -0,0 +1,13 @@
+package pl.baadamczyk.mtgmatchups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgmatchupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgmatchupsApplication.class, args);
+	}
+
+}
